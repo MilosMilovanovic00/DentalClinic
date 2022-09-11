10cc6595af08b3d835a7cdabdd52debcd0b2bd67
MilosMilovanovic00 <102919389+milosmilovanovic00@users.noreply.github.com>
2022-09-11 16:36:09 +0200

    Created react project

--- project/src/main/java/com/example/project/appointment/AnalysisType.java
+++ project/src/main/java/com/example/project/appointment/AnalysisType.java
@@ -1,5 +1,14 @@
 package com.example.project.appointment;
 
 public enum AnalysisType {
+    InitialExam("Initial Exam"),
+    DentalCheckup("Dental Checkup"),
+    ComprehensiveExamination("Comprehensive Examination"),
+    EmergencyCare("Emergency Care");
 
+    public final String label;
+
+    AnalysisType(String label) {
+        this.label = label;
+    }
 }
