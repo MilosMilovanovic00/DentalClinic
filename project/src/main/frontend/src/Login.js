f86718b6590ec4a96894e0d0c4ae83808ec2214b
MilosMilovanovic00 <102919389+MilosMilovanovic00@users.noreply.github.com>
2022-09-11 22:25:29 +0200

    Added UserProfile, Login as react component and made preview for login

---
+++ project/src/main/frontend/src/Login.js
@@ -0,0 +1,37 @@
+import colorPalette from "./index";
+import 'bootstrap/dist/css/bootstrap.css';
+import {Button, Form} from "react-bootstrap";
+import './colors.scss'
+import {useState} from "react";
+// import axios from "axios";
+
+export default function Login() {
+    const [email, setEmail] = useState();
+
+    // function logIn() {
+    // }
+    return (
+        <div className="d-flex flex-row vh-100" style={{backgroundColor: colorPalette.lightest}}>
+            <div className="d-flex justify-content-center w-75 m-5 rounded rounded-3">
+                <img className="img-fluid rounded rounded-3" src="/images/loginImage.jpg" alt={"login"}/>
+            </div>
+            <div className="w-25 m-5 rounded rounded-3 h-50" style={{backgroundColor: colorPalette.darkest}}>
+                <div className="d-flex flex-column m-3">
+                    <h1 className="title text-white ">Dental clinic</h1>
+                    <Form className="mt-5">
+                        <Form.Group className="my-3 mx-1" controlId="formBasicEmail">
+                            <Form.Label className="text-white fs-4">Email address</Form.Label>
+                            <Form.Control type="email" placeholder="Enter email"
+                                          onInput={event => setEmail(event.target.value)}/>
+                        </Form.Group>
+                    </Form>
+                    <div className="d-flex justify-content-center">
+                        <Button variant={"outline-lightest"} className={"w-50"}>Log in</Button>
+                    </div>
+                    <div className="d-inline mb-auto">
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
--- project/src/main/frontend/src/index.js
+++ project/src/main/frontend/src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+import {DevSupport} from "@react-buddy/ide-toolbox";
+import {ComponentPreviews, useInitial} from "./dev";
+import './colors.scss'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <DevSupport ComponentPreviews={ComponentPreviews}
+                    useInitialHook={useInitial}>
+            <App/>
+        </DevSupport>
+    </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const colorPalette={
+    darkest:"#46A094",
+    mediumDark:"#6BBD99",
+    mediumLight:"#AECFA4",
+    lightest:"#C4E8c2",
+}
+export default colorPalette
